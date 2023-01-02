d581bc667634d4b4270aeddcf92cfda38d3ae509
Damien Frigewski <damien.frigewski@aaa-internetshops.de>
2023-01-02 01:38:16 +0100

    change setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ setup(
     version='1.0',
     description='A library to simplify the access to the REST API of similarweb.',
     author='Damien Frigewski',
+    author_email='dfrigewski@gmail.com',
+    url='https://github.com/DamienDrash/similarweb_rest_api',
     packages=['similarweb_rest_api'],
-    install_requires=['requests'],
+    install_requires=['pandas', 'requests', 'lxml', 'json', 'yaml'],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     license='MIT',
-    author_email='dfrigewski@gmail.com',
 )
